c2e67e367fad7f70844bc828bd0d5c7d50bed567
Nguyen Tuan Anh <104725942+tuanemdev@users.noreply.github.com>
2024-06-23 18:59:50 +0700

    Multi target and dependencies

--- Sources/LearnSPM/LearnSPM.swift
+++ Sources/LearnSPM/LearnSPM.swift
@@ -2,11 +2,15 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
+import OtherTarget
 
 public struct LearnSPM {
     public init() { }
     
     public func printDes() {
         print("Learn Swift Package Manager")
+        let me = OtherTarget()
+        let myName = me.makeSomethingAwesome()
+        print(myName)
     }
 }
