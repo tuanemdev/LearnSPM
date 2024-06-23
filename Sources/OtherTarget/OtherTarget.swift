b0ac4bf5f2b8659bfe3ad70eb4d20c4dffb667b8
Nguyen Tuan Anh <104725942+tuanemdev@users.noreply.github.com>
2024-06-23 18:48:46 +0700

    create OtherTarget

---
+++ Sources/OtherTarget/OtherTarget.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nguyen Tuan Anh on 23/6/24.
+//
+
+import Foundation
+
+public struct OtherTarget {
+    public init() { }
+    
+    public func makeSomethingAwesome() -> String {
+        return "Tuan Em"
+    }
+}
