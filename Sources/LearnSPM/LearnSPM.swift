7e7eaadc1a6a4198bc2644b22c5b321aa7165e91
Nguyen Tuan Anh <104725942+tuanemdev@users.noreply.github.com>
2024-06-23 23:25:37 +0700

    version

--- Sources/LearnSPM/LearnSPM.swift
+++ Sources/LearnSPM/LearnSPM.swift
@@ -26,4 +26,12 @@ public struct LearnSPM {
  Best practices là tạo một subfolder Resources riêng để quản lý tài nguyên:
  + Nếu resources là: Interface Builder files (XIB, storyboards), Core Data files (xcdatamodeld), Asset catalogs, .lproj thì Xcode sẽ tự nhận dạng đó là file resources, ta không cần làm gì thêm
  + Nếu là file khác thì cần sửa file Package Manifest (ví dụ với file learn_spm.json)
+ 
+ Version của Package được quản lý bằng việc đánh Tag với Git, với Tag name là version cần đánh
+ Format có dạng: MAJOR.MINOR.PATCH
+ Đảm bảo đánh version tuân thủ theo nguyên tắc của Semantic Versioning 2.0.0: https://semver.org/
+ ---------- tóm gọn ----------
+ MAJOR: tăng khi có sự thay đổi về public API hiện có
+ MINOR: tăng khi bổ sung thêm public API
+ PATCH: tăng khi chỉ có sự thay đổi nội bộ của Package, không thêm, sửa gì public API
  */
