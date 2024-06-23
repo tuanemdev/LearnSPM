25796798bca50557e548bb4919222ebab0279728
Nguyen Tuan Anh <104725942+tuanemdev@users.noreply.github.com>
2024-06-23 18:42:40 +0700

    SPM target

--- Sources/LearnSPM/LearnSPM.swift
+++ Sources/LearnSPM/LearnSPM.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct LearnSPM {
+    public init() { }
+    
+    public func printDes() {
+        print("Learn Swift Package Manager")
+    }
+}
