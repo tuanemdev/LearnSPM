51021a1b917d9ad7ae48c4a22f905e7dbabea6da
Nguyen Tuan Anh <104725942+tuanemdev@users.noreply.github.com>
2024-06-30 15:14:26 +0700

    package access level

--- Sources/OtherTarget/OtherTarget.swift
+++ Sources/OtherTarget/OtherTarget.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 
-public struct OtherTarget {
-    public init() { }
+// package access level nhằm mục đích bộ lộ nó với một Target (module) khác nằm trong Package
+// nhưng không bộc lộ nó ra bên ngoài Package
+package struct OtherTarget {
+    package init() { }
     
-    public func makeSomethingAwesome() -> String {
+    package func makeSomethingAwesome() -> String {
         return "Tuan Em"
     }
 }
